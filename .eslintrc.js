ec7eec5bf90bdd79363e19e95e2661a280ba0908
dickwyn <dickwyn@outlook.com>
2019-08-31 23:23:54 -0700

    (feat) setup eslint and lint files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 100,
+      },
+    ],
+  },
+  plugins: ['prettier'],
+  env: {
+    jest: true,
+  },
+}
