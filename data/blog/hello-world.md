81785f8382650d957fce7629d5fe34eb27ce1dbd
dickwyn <dickwyn@outlook.com>
2019-11-09 20:10:44 -0700

    (config) gatsby remark relative images plugin

--- data/blog/hello-world.md
+++ data/blog/hello-world.md
@@ -5,7 +5,7 @@ title: Hello World
 date: 2019-08-31T07:46:06.901Z
 description: My first post
 featuredpost: true
-featuredimage: /src/images/sara-riano-hello.jpg
+featuredimage: /images/sara-riano-hello.jpg
 tags:
   - gatsby
   - netlify
--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,15 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
     `gatsby-plugin-dark-mode`,
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'uploads',
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -31,6 +38,30 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // gatsby-remark-relative-images must
+          // go before gatsby-remark-images
+          {
+            resolve: `gatsby-remark-relative-images`,
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
