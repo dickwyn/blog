2a06c53ecd1ad8dbe4af816ebd67d0a17633d4d0
dickwyn <dickwyn@outlook.com>
2019-11-10 19:59:49 -0700

    (fix) replace boundActionCreators to actions

--- gatsby-node.js
+++ gatsby-node.js
@@ -54,8 +54,8 @@ exports.createPages = ({ actions, graphql }) => {
   });
 };
 
-exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
-  const { createNodeField } = boundActionCreators;
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
   fmImagesToRelative(node);
 
   if (node.internal.type === `MarkdownRemark`) {
