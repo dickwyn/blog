718a8dc5e01929cb46628c94ae61d80887a7f5b6
dickwyn <dickwyn@outlook.com>
2019-11-10 21:27:56 -0700

    (feat) add development alert banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,22 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Home" />
       <div className="content">
+        <div className="banner">
+          <p>
+            <span role="img" aria-labelledby="construction emoji">
+              🚧
+            </span>
+            Site is still under construction. All content is just test data for development purposes
+            only. View{' '}
+            <a href="https://www.dickwyn.wordpress.com" target="_blank" rel="noopener noreferrer">
+              https://www.dickwyn.wordpress.com
+            </a>{' '}
+            for old blog posts.{' '}
+            <span role="img" aria-labelledby="construction emoji">
+              🚧
+            </span>
+          </p>
+        </div>
         {posts.map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
