01a74146caa55c986e2d838f4f34e7d1ab1a3a5b
dickwyn <dickwyn@outlook.com>
2019-11-23 23:53:05 -0700

    (feat) route existing wordpress traffic back to wordpress

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'gatsby';
+import { Location } from '@reach/router';
+import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <div className="content">
-      <h1>404</h1>
-      <h2>Something&apos;s wrong</h2>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <p>
-        Go back <Link to="/">home</Link>
-      </p>
-    </div>
-  </Layout>
-);
+class NotFoundPage extends Component {
+  componentDidMount() {
+    const {
+      location: { pathname },
+    } = this.props;
 
-export default NotFoundPage;
+    console.log(`https://dickwyn.wordpress.com${pathname}`);
+    if (pathname.includes('/20')) {
+      global.window.location.href = `https://dickwyn.wordpress.com${pathname}`;
+    }
+  }
+
+  render() {
+    const {
+      location: { href, origin, pathname },
+    } = this.props;
+    return (
+      <Layout>
+        <SEO title="404: Not found" />
+        <div className="content">
+          <h1>404 - Not found</h1>
+          <h2>Something&apos;s wrong</h2>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <h3>Diagnostic information</h3>
+          <p>href: {href}</p>
+          <p>origin: {origin}</p>
+          <p>pathname: {pathname}</p>
+          <br />
+          <p>
+            Go back <Link to="/">home</Link>
+          </p>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+NotFoundPage.propTypes = {
+  location: PropTypes.shape({
+    href: PropTypes.string,
+    origin: PropTypes.string,
+    pathname: PropTypes.string,
+  }).isRequired,
+};
+
+export default props => (
+  <Location>{locationProps => <NotFoundPage {...locationProps} {...props} />}</Location>
+);
