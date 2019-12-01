960cb2482d91dfcde0f564e656414382e4bbceb1
dickwyn <dickwyn@outlook.com>
2019-12-01 00:54:16 -0700

    (feat) remove all production posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ import getDevelopmentPosts from '../hooks/get-development-posts';
 import PostPreview from '../components/PostPreview';
 
 const IndexPage = () => {
-  const posts = process.env.NODE_ENV === 'development' ? getDevelopmentPosts() : getPosts();
+  const posts = process.env.NODE_ENV !== 'development' ? getDevelopmentPosts() : getPosts();
 
   return (
     <Layout>
       <SEO title="Home" />
       <div className="content">
-        {posts.map(post => (
-          <PostPreview key={post.slug} post={post} />
-        ))}
+        {posts.length > 0
+          ? posts.map(post => <PostPreview key={post.slug} post={post} />)
+          : 'new posts coming soon. stay tuned.'}
       </div>
     </Layout>
   );
